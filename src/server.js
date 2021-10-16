493c1a6bf0fb095caa06f1efc9c6c832164da168
pedrolssilva <pedrolssilva@hotmail.com>
2021-10-16 15:16:54 -0300

    handling errors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("dotenv-safe").config();
+require('express-async-errors');
 const express = require('express');
 const cors = require("cors");
 const routes = require("./routes");
@@ -9,5 +10,9 @@ server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(routes);
+
+server.use((error, req, res, next) => {
+  res.status(500).json(error);
+})
 
 server.listen(5000, () => { console.log('Server is running')})
