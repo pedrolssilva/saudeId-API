10439059674d69ad4354e650f96dcd66dce7fb28
pedrolssilva <pedrolssilva@hotmail.com>
2021-10-16 00:23:59 -0300

    Using cors to allow any origin

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require("dotenv-safe").config();
 const express = require('express');
+const cors = require("cors");
 const routes = require("./routes");
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(routes);
 
-server.listen(3000, () => { console.log('Server is running')})
+server.listen(5000, () => { console.log('Server is running')})
